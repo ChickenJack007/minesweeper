100fa23ca99d1b23bc2716cff817f9891a0314c2
Jack Melson <j1melson@outlook.com>
2026-04-10 10:26:55 -0500

    allowed the user to controll the width, height and bombs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from scripts.utility import gen_board, print_board
-
-board = gen_board(30,16,99)
+height, width, bombs = input("").split()
+board = gen_board(int(height),int(width),int(bombs))
 print_board(board)
